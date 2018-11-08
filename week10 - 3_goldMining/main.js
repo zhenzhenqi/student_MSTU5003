9ec3b28ab94f3bfa579bdd3bca22316b1a2b36dd
zhenzhenqi <zhenzhenqi@gmail.com>
2018-11-08 02:10:17 -0500

    ghost game

--- week10 - 3_goldMining/main.js
+++ week10 - 3_goldMining/main.js
@@ -8,10 +8,13 @@ var height = canvas.height = window.innerHeight;
 var background = new Image();
 background.src = "images/spiritedAway.jpg";
 
-background.onload = function(){
-    ctx.drawImage(background,0,0);
-}
+var mouseX = width/2;
+var mouseY = height*0.2;
 
+function moveGhost(e){
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+}
 // function to generate random number
 function random(min,max) {
   var num = Math.floor(Math.random()*(max-min)) + min;
@@ -22,71 +25,100 @@ function random(min,max) {
 function Gold() {
   this.x = random(0, width);
   this.y = random(0, width);
-  this.velX = 10;
-  this.velY = 10;
+  this.velX = random(-3, 3);
+  this.velY = random(-3, 3);
   var img = new Image();
   img.src = 'images/potOfGold.png';
   this.img = img;
-
+  this.sizeX = img.naturalWidth*0.8;
+  this.sizeY = img.naturalHeight*0.8;
 }
 
 // define ball draw method
 Gold.prototype.draw = function() {
-  ctx.drawImage(this.img,10,10);
+  ctx.drawImage(this.img, this.x, this.y, this.sizeX, this.sizeY);
 };
 
 Gold.prototype.reset = function() {
-    this.x = random(0, width);
-    this.y = random(0, height);
+    this.x = random(this.sizeX, width-this.sizeX);
+    this.y = random(this.sizeY, height-this.sizeY);
 };
 
 Gold.prototype.update = function() {
-  if((this.x + this.size) >= width) {
-    this.velX = -(this.velX);
+  if((this.x + this.sizeX) >= width) {
+    this.x = this.sizeX;
   }
 
-  if((this.x - this.size) <= 0) {
-    this.velX = -(this.velX);
+  if((this.x - this.sizeX) <= 0) {
+    this.x = width-this.sizeX;
   }
 
-  if((this.y + this.size) >= height) {
-    this.velY = -(this.velY);
+  if((this.y + this.sizeY) >= height) {
+    this.y = this.sizeY;
   }
 
-  if((this.y - this.size) <= 0) {
-    this.velY = -(this.velY);
+  if((this.y - this.sizeY) <= 0) {
+    this.y = height-this.sizeY;
   }
 
-  this.x += this.velX;
-  this.y += this.velY;
+  this.x += random(-10, 10);
+  this.y += random(-10, 10);
 };
 
-// define ball collision detection
 
-Gold.prototype.collisionDetect = function(_ghost) {
-      var ghost = _ghost;
-      var dx = this.x - balls[j].x;
-      var dy = this.y - balls[j].y;
+
+function Ghost() {
+  var img = new Image();
+  img.src = 'images/ghost.png';
+  this.img = img;
+  this.sizeX = img.naturalWidth*0.8;
+  this.sizeY = img.naturalHeight*0.8;
+  this.x = mouseX-this.sizeX;
+  this.y = mouseY-this.sizeY;
+}
+
+Ghost.prototype.draw = function() {
+  ctx.drawImage(this.img, this.x, this.y, this.sizeX, this.sizeY);
+};
+
+
+Ghost.prototype.collide = function( _target) {
+      var target = _target;
+      var dx = target.x - this.x;
+      var dy = target.y - this.y;
+
       var distance = Math.sqrt(dx * dx + dy * dy);
 
-      if (distance < this.size + ghost.size) {
-        this.reset();
+      if (distance < this.sizeX + target.sizeX) {
+        target.reset();
       }
 }
 
+Ghost.prototype.update = function(){
+    var dx = mouseX - (this.x+this.sizeX/2);
+    var dy = mouseY - (this.y+this.sizeY/2);
+    console.log(mouseX);
 
-// define array to store balls
+    this.x += dx*0.01;
+    this.y += dy*0.01;
+}
 
 var myGold = new Gold();
+var myGhost = new Ghost();
 
 // define loop that keeps drawing the scene constantly
 function loop() {
+    ctx.drawImage(background,0,0);
+
     myGold.draw();
     myGold.update();
-    //myGol.collisionDetect();
+
+    myGhost.draw();
+    myGhost.update();
+    myGhost.collide(myGold);
+    //console.log(myGhost.x);
   //call for a new animation frame to be drawn on the screen
   requestAnimationFrame(loop);
 }
-
 
 loop();
